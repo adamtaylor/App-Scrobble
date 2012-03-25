d19a095ea926833dd9e9e80b0ba5006cf6e8c312
Adam Taylor <adamjctaylor@gmail.com>
2012-03-25 13:24:20 +0100

    Add POD to explain the role

--- lib/App/Scrobble/Role/WithService.pm
+++ lib/App/Scrobble/Role/WithService.pm
@@ -30,19 +30,33 @@ __END__
 
 =pod
 
-=head1 SYNOPSIS
-
 =head1 DESCRIPTION
+
+Interface for L<App::Scrobble::Service::*> plugins.
 
 =head1 ATTRIBUTES
 
+=head2 C<url>
+
+The URL (Str) of the podcast/cloudcast/webpage/whatever to scrobble.
+
 =head2 C<tracks>
+
+Arrayref of track data in the form:
+
+    { title => 'foo', artist => 'bar' }
 
 =head1 METHODS
 
-=head2 C<is_plugin_for>>
+=head2 C<is_plugin_for>
+
+Will be passed the URL to scrobble and should return a boolean indicating
+whether this plugin can scrobble this URL.
 
 =head2 C<get_tracks>
+
+Should populate the C<tracks> hashref with the data from the URL. Is expected
+to return the C<tracks> hashref.
 
 =head1 SEE ALSO
 
