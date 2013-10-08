0498ff936cf2c5be6ffc13b4d9b3f69c286b678a
Adam Taylor <adamjctaylor@gmail.com>
2013-10-08 08:11:37 +0100

    FInish BBC scrobbler

--- lib/App/Scrobble/Service/BBC.pm
+++ lib/App/Scrobble/Service/BBC.pm
@@ -1,9 +1,13 @@
+# ABSTRACT:
 package App::Scrobble::Service::BBC;
 
 use Moose;
+use namespace::autoclean;
 with 'App::Scrobble::Role::WithService';
 
-use WebService::BBC::TrackListings;
+use WWW::BBC::TrackListings;
+
+# VERSION
 
 sub is_plugin_for {
     my $class = shift;
@@ -17,6 +21,30 @@ sub is_plugin_for {
 sub get_tracks {
     my $self = shift;
 
-    my $track_listings = WebService::BBC::TrackListings->new( $self->url );
-    return $track_listings->all_tracks();
+    my $track_listings = WWW::BBC::TrackListings->new({ url => $self->url });
+    my @tracks = $track_listings->all_tracks();
+    return \@tracks;
 }
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
+
+=pod
+
+=head1 DESCRIPTION
+
+L<App::Scrobble> plugin for L<BBC Radio Programmes|http://www.bbc.co.uk/radio/>/
+
+=head1 METHODS
+
+=head2 C<is_plugin_for>
+
+Returns a boolean indicating if the URL passed in is a BBC URL.
+
+=head2 C<get_tracks>
+
+Uses L<WWW::BBC::TrackListings> to construct return an array of track data.
+
+=cut
